26280793c50d20c80e07ec99d977e235c1df67c8
Nisanth <smvmvenunisandhan6e17@gmail.com>
2026-06-15 18:13:11 +0530

    Added employee routes

---
+++ src/routes/employeeRoutes.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+const router = express.Router();
+
+const {
+    createEmployee ,
+    getAllEmployees ,
+    getEmployeeById ,
+    updateEmployee ,
+    deleteEmployee ,
+} = require('../controllers/employeeController');
+
+const { validateEmployee } = require('../middlewares/validator');
+
+router
+      .route('/')
+      .get(getAllEmployees)
+      .post(validateEmployee, createEmployee);
+
+router
+      .route('/"id')
+      .get(getEmployeeById)
+      .put(validateEmployee, updateEmployee)
+      .delete(deleteEmployee);
+
+module.exports = router ;
