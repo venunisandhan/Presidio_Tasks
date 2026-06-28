7b910c21b1388cea6b1b5de2b9253f1836c503b5
venunisandhan <venunisandhan@gmail.com>
2026-06-28 16:26:03 +0530

    created server.js

---
+++ Week 4/src/server.js
@@ -0,0 +1,39 @@
+//Simple Product Catalog
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const items = [{ id:1 , name : 'Apple' , price : 30}];
+let nextID = 2;
+
+app.get('/health', (req,res)=>{
+    res.json({status:'ok', uptime:process.uptime()});
+});
+
+app.get('/items', (req,res)=>{
+    res.json({data : items});
+});
+app.get('/items/:id' , (req,res)=>{
+    const item = items.find(i => i.id === parseInt(req.params.id));
+    if(!item) return res.status(404).json({error : 'not found'});
+    res.json({data:item});
+});
+
+app.post('/items',(req,res)=>{
+    const {name,price} = req.body;
+    if(!name || !price) return res.status(400).json({error : 'name and price required'});
+    const newItem = {id : nextID++ , name , price };
+    items.push(newItem);
+    res.status(201).json({data:newItem});
+});
+
+const PORT = process.env.PORT || 3000;
+if(require.main == module)
+{
+    app.listen(PORT,()=>console.log(`Running on ${PORT}`));
+}
+
+module.exports = app;
